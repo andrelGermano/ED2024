ff46fec0d9e2db2817c67a242af5925cce592449
Lucas <lucasmaiagermano@gmail.com>
2024-07-10 21:28:34 -0300

    Adicionando BubbleSort

---
+++ __tests__/sort/BubbleSort.test.js
@@ -0,0 +1,18 @@
+import { BubbleSort } from "../../src/sort/BubbleSort.js";
+
+let list;
+
+test("Melhor caso", () => {
+    list = [1, 2, 3, 4, 5, 7, 6, 8, 9, 10];
+    expect(BubbleSort(list)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
+
+test("Pior caso", () => {
+    list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+    expect(BubbleSort(list)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
+
+test("Lista vazia", () => {
+    list = [];
+    expect(() => BubbleSort(list)).toThrow("Empty list");
+});
---
+++ src/sort/BubbleSort.js
@@ -0,0 +1,19 @@
+// src/sort/BubbleSort.js
+export function BubbleSort(list) {
+    if (list.length === 0) {
+        throw new Error("Empty list");
+    }
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < list.length - 1; i++) {
+            if (list[i] > list[i + 1]) {
+                let temp = list[i];
+                list[i] = list[i + 1];
+                list[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return list;
+}
